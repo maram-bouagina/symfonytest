3f624d4c3d4a5e198ef760a58e8eb5813265e7fc
Your Name <bouaginamaram6@gmail.com>
2025-04-23 17:24:28 +0100

    tp2

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/{name}', name: 'homepage')]
+    public function home(string $name): Response
+    {
+        return $this->render('index.html.twig', ['name' => $name]);
+    }
+}
